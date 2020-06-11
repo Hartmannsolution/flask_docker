33ab56a6471397dc4b61d8968131d734248a0831
Thomas Hartmann (THA - Lektor - Cphbusiness) <tha@cphbusiness.dk>
2020-06-11 15:38:36 +0200

    second version of recipes.json

--- flask/app/views.py
+++ flask/app/views.py
@@ -1,6 +1,10 @@
 from app import app
 
 import os
+import json
+
+with open('recipes.json','r') as jsonfile:
+        diction = json.load(jsonfile)
 
 @app.route("/")
 def index():
@@ -12,3 +16,11 @@ def index():
         return f"Hello from {app_name} running in a Docker container behind Nginx!"
 
     return "Hello from Flask"
+
+@app.route("/recipes")
+def recipes():
+    return diction['recipes']
+
+@app.route("/recipe/<id>")
+def recipe(id):
+    return diction['recipes'][id]
